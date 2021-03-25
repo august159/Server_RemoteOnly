61b5797bd19576c4427ebb979576c57fedfdab27
august159 <augustin.leman@gmail.com>
2021-03-25 18:48:35 +0100

    User model completed

--- models/JobOffer.js
+++ models/JobOffer.js
@@ -1,61 +1,47 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const jobofferShema = new Schema({
+const jobofferShema = new Schema(
+  {
     title: { type: String, required: true },
-    salary:  { type: Number, required: true },
+    salary: { type: Number, required: true },
     contractType: {
-        type: String,
-        enum: [
-          "CDI",
-          "CDD",
-          "Freelance",
-          "Stage",
-          "Alternance",
-          
-        ],
-        required: true,
-      },
-    fieldWork : {
-        type: String,
-        enum: [
-          "Produit",
-          "Design",
-          "Programmation",
-          "Customer Success",
-          "DevOps",
-          "Sales et Marketing",
-        ],
-        required: true,
-      },
-      profilDescription : {type:String} ,
-      jobDescription  : {type:String, required:true} ,
-      recruitmentProcess  : {type:String} ,
-      startingDate  : {type:Date} ,
-      status  : {type: Boolean,
-      enum: [
-        "true",
-        "false",
-      ]},
-      applications: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "applications",
-        },
-      ],
-      companies: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "companies",
-        },
-      ],
+      type: String,
+      enum: ["CDI", "CDD", "Freelance", "Stage", "Alternance"],
+      required: true,
     },
-    { timestamps: true },
-)
-        
-
-const ApplicationModel = mongoose.model("applications", applicationSchema);
-module.exports = ApplicationModel;
+    fieldWork: {
+      type: String,
+      enum: [
+        "Produit",
+        "Design",
+        "Programmation",
+        "Customer Success",
+        "DevOps",
+        "Sales et Marketing",
+      ],
+      required: true,
+    },
+    profilDescription: { type: String },
+    jobDescription: { type: String, required: true },
+    recruitmentProcess: { type: String },
+    startingDate: { type: Date },
+    status: { type: Boolean, enum: ["true", "false"] },
+    applications: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "applications",
+      },
+    ],
+    companies: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "companies",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const JobOfferModel = mongoose.model("joboffers", jobofferShema);
 module.exports = JobOfferModel;
--- models/User.js
+++ models/User.js
@@ -4,6 +4,36 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
+  avatar: String,
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  role: { type: String, enum: ["recruiter", "candidate"], required: true },
+  position: String,
+  companies: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "companies",
+    },
+  ],
+  phone: {
+    type: Number,
+    validate: {
+      validator: function (number) {
+        return /d{10}/.test(number);
+      },
+      message: "{VALUE} is not a valid 10 digit number!",
+    },
+  },
+  linkedIn: String,
+  gitHub: String,
+  otherWebsite: String,
+  additionalInfo: String,
+  joboffers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "joboffers",
+    },
+  ],
 });
 
 const UserModel = mongoose.model("users", userSchema);
