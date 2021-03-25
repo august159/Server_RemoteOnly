2c00a43ebaddb13ca55e085b64c36a9de2efed5e
august159 <augustin.leman@gmail.com>
2021-03-25 18:17:26 +0100

    Application model created

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+const UserModel = mongoose.model("users", userSchema);
+module.exports = UserModel;
